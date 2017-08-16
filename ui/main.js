bd79ebbdf0438e7c9441caf6d81eb1ad20104ac3
arjbht <30808841+arjbht@users.noreply.github.com>
2017-08-16 23:51:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ var name =nameInput.value;
 var submit =document.getElementById("submit.btn");
 submit.onclick=function(){
     
-    var names=["names1","names2","names3","names4"];
+    var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<=names.length;i++){
         list+= '<li>' + names[i] + '</li>';
