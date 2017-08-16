d613ea573be3e925476b543c691badf49e634a02
arjbht <30808841+arjbht@users.noreply.github.com>
2017-08-16 23:46:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,17 @@ button.onclick=function(){
     var span=document.getElementById("count");
     span.innerHTML=count.toString;
 };
+var nameInput=document.getElementById("name");
+var name =nameInput.value;
+var submit =document.getElementById("submit.btn");
+submit.onclick=function(){
+    
+    var names=["names1","names2","names3","names4"];
+    var list='';
+    for(var i=0;i<=names.length;i++){
+        list+= '<li>' + names[i] + '</li>';
+        
+    }
+    var ul=document.getElementById("namelist");
+    ul.innerHtml="list";
+};
